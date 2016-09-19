24e24a0659654ffa4813b25704d4306e1f4f5ed0
JPolfliet <jeroen.polfliet@altran.com>
2016-09-19 16:49:59 +0200

    last commit day1

--- day1/ex3/temperature_tester.py
+++ day1/ex3/temperature_tester.py
@@ -5,17 +5,17 @@ class Test_temperature(unittest.TestCase):
     def test_K2C(self):
         a = 50
         b = temperature.convCtoK(a)
-        self.assertEqual(4,temperature.convKtoC(b))
+        self.assertEqual(a,temperature.convKtoC(b))
     def test_F2C(self):
         a = 50
         b = temperature.convCtoF(a)
-        self.assertEqual(4,temperature.convFtoC(b))
+        self.assertEqual(a,temperature.convFtoC(b))
     def test_K2F(self):
         a = 50
         b = temperature.convKtoC(a)
         c = temperature.convCtoF(b)
-        self.assertEqual(4,temperature.convCtoK(b))
-        self.assertEqual(4,temperature.convFtoC(c))
+        self.assertEqual(a,temperature.convCtoK(b))
+        self.assertEqual(b,temperature.convFtoC(c))
 
 
 
