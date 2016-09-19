c9027a13970e84728c0f9e308fe76eb047b5f730
JPolfliet <jeroen.polfliet@altran.com>
2016-09-19 11:50:28 +0200

    first commit day1

---
+++ day1/ex1/ex1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/encv python
+print ("Hello World!")
+a=3+4+2j
+print a
+b=4+2j-7-8j
+print b
+c=1.0/3
+print c
+d=2**8
+print d
+s='a cool string this is!'
+print s
+print len(s)
+print s[4]
+print s[-2]
+print s[-5:]
+print ord(s[0])
+s=s.replace('i','a')
+print s
